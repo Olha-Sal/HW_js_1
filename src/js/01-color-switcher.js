64a6d10f8eed82fcba78c4a866e1740d3d67b25e
Olha-Sal <kosyuk.o@gmail.com>
2023-11-07 22:35:43 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const htmlBody = document.querySelector("body");
+let intervalColorId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+startBtn.addEventListener("click", () => {
+  intervalColorId = setInterval(() => {
+  htmlBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  stopBtn.removeAttribute("disabled");
+  startBtn.setAttribute("disabled", true);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(intervalColorId);
+  startBtn.removeAttribute("disabled");
+  stopBtn.setAttribute("disabled", true);
+});
